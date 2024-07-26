9e29c30d62840b7f629f9624258b1c8ebe7bc05b
Denys209818 <deniskravcuk63@gmail.com>
2024-07-27 02:15:56 +0300

    make build

---
+++ index.ffb351c9.js
@@ -0,0 +1,2 @@
+!function e(n,r){for(var t in r){var a=0===Object.keys(r[t]).length,c=document.createElement("li");if(c.innerHTML=t,!a){var i=document.createElement("ul");c.append(i),e(i,r[t])}n.append(c)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.ffb351c9.js.map
